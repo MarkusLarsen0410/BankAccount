f0aa5ad1e95185af45c4b5e1d1f894585a45c5d3
markuslarsen <Bachlarsen@live.com>
2025-11-13 13:24:56 +0100

    Oprettelse af bankAccoount klasse

---
+++ src/BankAccount.java
@@ -0,0 +1,34 @@
+public class BankAccount {
+
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+
+        balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+
+        setBalance(balance + amount);
+    }
+
+    public void withDraw(double amount) {
+
+
+        setBalance(balance - amount);
+    }
+
+    public double getBalance() {
+
+        return balance;
+    }
+
+    private void setBalance(double balance) {
+
+        if (balance > 0) {
+
+            this.balance = balance;
+        }
+    }
+
+}
